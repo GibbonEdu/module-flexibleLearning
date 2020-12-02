5d3b0139ebe230aab170684d5dd1a9b937a9cbe7
Sandra Kuipers <sandra@skuipers.com>
2020-12-02 10:05:02 +0800

    Fix version numbers

--- Flexible Learning/manifest.php
+++ Flexible Learning/manifest.php
@@ -25,7 +25,7 @@ $description = 'View, create and manage Flexible Learning units.';
 $entryURL    = "units_browse.php";
 $type        = "Additional";
 $category    = 'Learn';
-$version     = '0.3.01';
+$version     = '0.4.00';
 $author      = 'Harry Merrett';
 $url         = '';
 

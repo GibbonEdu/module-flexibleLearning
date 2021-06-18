0933b9d2d0a8e504654fb4cbad357420f663b313
Sandra Kuipers <sandra@skuipers.com>
2021-06-18 15:02:38 +0800

    Update units_browse_details_feedback.php

--- Flexible Learning/units_browse_details_feedback.php
+++ Flexible Learning/units_browse_details_feedback.php
@@ -103,7 +103,7 @@ if (isActionAccessible($guid, $connection2, '/modules/Flexible Learning/units_br
     $table->addColumn('category', __('Category'));
 
     $table->addColumn('author', __('Author'))
-        ->format(function ($person) use ($guid, $connection2, $gibbon) {
+        ->format(function ($person) use ($guid, $connection2, $session) {
             if ($person['status'] == 'Full' && isActionAccessible($guid, $connection2, '/modules/Staff/staff_view_details.php')) {
                 return "<a href=" . $session->get('absoluteURL') . "/index.php?q=/modules/Staff/staff_view_details.php&gibbonPersonID=" . $person['gibbonPersonIDCreator'] . ">" . Format::name('', $person['preferredName'], $person['surname'], 'Staff', false, true) . "</a>";
             } else {

2a92aabfbe85fe67bc3f0e0027330ac38d153193
Ali Alam <aalam@ichk.edu.hk>
2026-04-27 14:56:36 +0800

    Improved the version number

--- Flexible Learning/CHANGEDB.php
+++ Flexible Learning/CHANGEDB.php
@@ -292,8 +292,8 @@ $sql[$count][1] = "";
 $sql[$count][0] = '1.3.01';
 $sql[$count][1] = "";
 
-//v1.3.02
+//v1.4.00
 ++$count;
-$sql[$count][0] = '1.3.02';
+$sql[$count][0] = '1.4.00';
 $sql[$count][1] = "";
 
--- Flexible Learning/manifest.php
+++ Flexible Learning/manifest.php
@@ -27,7 +27,7 @@ $description = 'View, create and manage Flexible Learning units.';
 $entryURL    = "units_browse.php";
 $type        = "Additional";
 $category    = 'Learn';
-$version     = '1.3.02';
+$version     = '1.4.00';
 $author      = "Gibbon Foundation";
 $url         = "https://gibbonedu.org";
 
